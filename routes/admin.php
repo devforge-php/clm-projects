9a56e46f5150b04ae180d88eec678e5a4324c367
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-04-03 18:13:27 +0500

    Hozirgi o'zgarishlarni saqladim

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::resource('tasks', TaskController::class)->middleware('throttle:60,1');
    Route::get('user', [UserController::class, 'index']);
-   Route::get('user/{id}', [UserController::class, 'show']);
+   Route::get('user/{id}', [UserController::class, 'show'])->name('show');
    Route::delete('user/{id}', [UserController::class, 'destroy']);
     Route::get('SocialMedia', [SocialUserNamesController::class, 'index'])->middleware('throttle:30,1');
 
