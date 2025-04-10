d9a0a26bf5be9d77ae20736d7a1cc9f604fb9ca4
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-04-10 22:26:18 +0500

    clm

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,6 +45,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-    //    code saved messageresishaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+    //    code saved messageresishaaaaa
+    // nega bunday bolyabdi
     }
 }
