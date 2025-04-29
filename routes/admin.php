42b6a66da4488f044bf6db263e4ad79c531378d1
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-04-29 21:42:04 +0500

    clm

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\Posts\PostController;
 use App\Http\Controllers\Admin\SocialMediaUserNames\SocialUserNamesController;
 use App\Http\Controllers\Admin\Tasks\TaskController;
 use App\Http\Controllers\Admin\UserResetController;
@@ -7,16 +8,28 @@ use App\Http\Controllers\Admin\Users\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    
+    // Throttled routes
     Route::middleware('throttle:60,1')->group(function () {
-        Route::get('/tasks', [TaskController::class, 'index']);         // GET: barcha tasklar
-        Route::get('/tasks/{id}', [TaskController::class, 'show']);     // GET: bitta task
-        Route::post('/tasks', [TaskController::class, 'store']);        // POST: task yaratish
-        Route::post('/tasks/{id}', [TaskController::class, 'update']);  // POST: update uchun (PUT emas)
-        Route::delete('/tasks/{id}', [TaskController::class, 'destroy']); // DELETE: task o‘chirish
+        // Tasks
+        Route::get('/tasks', [TaskController::class, 'index']);
+        Route::get('/tasks/{id}', [TaskController::class, 'show']);
+        Route::post('/tasks', [TaskController::class, 'store']);
+        Route::post('/tasks/{id}', [TaskController::class, 'update']);
+        Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
+
+        // Posts
+        Route::get('/posts', [PostController::class, 'index']);
+        Route::get('/posts/{id}', [PostController::class, 'show']);
+        Route::post('/posts', [PostController::class, 'store']);
+        Route::post('/posts/{id}', [PostController::class, 'update']);
+        Route::delete('/posts/{id}', [PostController::class, 'destroy']);
     });
-   Route::get('user', [UserController::class, 'index']);
-   Route::get('user/{id}', [UserController::class, 'show'])->name('show');
-   Route::delete('user/{id}', [UserController::class, 'destroy']);
+
+    // No throttle
+    Route::get('user', [UserController::class, 'index']);
+    Route::get('user/{id}', [UserController::class, 'show'])->name('show');
+    Route::delete('user/{id}', [UserController::class, 'destroy']);
     Route::get('SocialMedia', [SocialUserNamesController::class, 'index'])->middleware('throttle:30,1');
 
     Route::post('reset-users', [UserResetController::class, 'resetAllUsers'])->middleware('throttle:5,1');
